203c59fcefe8f21a4b882af4341bcf706c9842ae
Jean-Christophe Houde <jean.christophe.houde@gmail.com>
2018-03-08 13:46:52 -0500

    Tried fixing the urlresolver import.

--- simpleblog/models.py
+++ simpleblog/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils.encoding import python_2_unicode_compatible
